18acd12d3dee42af71bf1833d76ee062310479df
hauy1 <hauy1@stconline.edu.hk>
2025-12-01 23:37:26 +0800

    day 1
    idea for part 1:
    check when the running total is 0 mod 100
    
    idea for part 2:
    when the dial is at 0, count increases by (units // 100) times
    use this fact by adjusting the dial to 0 and decreasing the units

---
+++ day1.py
@@ -0,0 +1,46 @@
+with open('day1.txt') as f:
+    instructions = f.read().splitlines()
+
+# part 1
+count = 0
+dial = 50
+for instruction in instructions:
+    if instruction[0] == 'L':
+        dial -= int(instruction[1:])
+    else:
+        dial += int(instruction[1:])
+    if dial % 100 == 0:
+        count += 1
+
+print(f"Part 1: {count}")
+
+# part 2
+count = 0
+dial = 50
+for instruction in instructions:
+    dir = instruction[0]
+    units = int(instruction[1:])
+
+    if dir == 'L':
+        end = (dial - units) % 100
+        if dial != 0:
+            for i in range(100):
+                dial -= 1
+                if dial == 0:
+                    units -= i + 1
+                    count += 1
+                    break
+    
+    else:
+        end = (dial + units) % 100
+        for i in range(100):
+            dial += 1
+            if dial == 100:
+                units -= i + 1
+                count += 1
+                break
+    
+    count += units // 100
+    dial = end
+
+print(f"Part 2: {count}")
